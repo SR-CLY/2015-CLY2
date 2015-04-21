3ed0f607eb81dd0469dcf6e70d99977d06135b2b
cly_js1 <cly_js1@srobo.org>
2015-04-21 14:23:53 +0000

    Modified test parameter

--- robot.py
+++ robot.py
@@ -11,7 +11,8 @@ def stage_one(robot, power):
     drive(robot, 1, power)
     release_marker(robot, power)
 
+def test(robot, power):
+    carpet_test(robot, power)
 
-stage_one(R, 100)
+test(R, 100)
 
-stop(R)
