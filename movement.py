9f4ef7f3fc9a9a871a782e58da0e13b401f15a6e
cly_js1 <cly_js1@srobo.org>
2015-04-22 14:50:56 +0000

    Changed compensation

--- movement.py
+++ movement.py
@@ -9,7 +9,7 @@ from time import sleep
 
 TRACK = 4  # Width of wheel in mm
 SPEED = 580  # Speed in mm/s
-RIGHT_COMPENSATION = 1 # Multiplier used to counter the effects of the right motor being more powerful 
+RIGHT_COMPENSATION = 0.98 # Multiplier used to counter the effects of the right motor being more powerful 
 FULL_TURN = 1.975  # Seconds
 CARPET_CONSTANT = 0 # Varies with thickness of carpet/ type of surface
 DISTANCE_TRAVELLED_SECOND = 0.4 #Distance in metres travelled per second
