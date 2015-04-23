bab1244bca8ec602fc33e8b692e3e89a0fb67bfa
cly_js1 <cly_js1@srobo.org>
2015-04-23 11:55:01 +0000

    Created saftey code

--- robot.py
+++ robot.py
@@ -12,6 +12,11 @@ def stage_one(robot, log, power):
     drive_to_marker(robot, log, power)
     drive(robot, log, 1, power)
     release_marker(robot, power)
+    
+def saftey_code(robot,log, power):
+    drive_distance(robot, log, 2, power)
+    turn(robot, log, 45, power)
+    drive(robot,log, 3, power)
 
 def test(robot, log, power):
     stage_one(robot, log, power)
