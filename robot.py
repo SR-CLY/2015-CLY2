d6aca10dbfb3526ad8df762df31c2f985dcdfe5d
cly_js1 <cly_js1@srobo.org>
2015-04-23 10:54:17 +0000

    Setup logging

--- robot.py
+++ robot.py
@@ -1,18 +1,20 @@
 from sr.robot import *
 from time import sleep
 from strategy import *
+from logger import *
+log = Logger()
 
 """Main File"""
 
 R = Robot()
 
-def stage_one(robot, power):
-    drive_to_marker(robot, power)
+def stage_one(robot, log, power):
+    drive_to_marker(robot, log, power)
     drive(robot, 1, power)
     release_marker(robot, power)
 
-def test(robot, power):
-    stage_one(robot, power)
+def test(robot, log, power):
+    stage_one(robot, log, power)
 
 test(R, 100)
 
