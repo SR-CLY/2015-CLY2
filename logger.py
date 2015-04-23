c53b22bbedf18a1f1a37330d5450dfb2f2ab7fb2
cly_js1 <cly_js1@srobo.org>
2015-04-23 09:11:30 +0000

    Modified Logger to be able to retrace movement

--- logger.py
+++ logger.py
@@ -1,16 +1,21 @@
-class Loggger():
+class Logger():
     def __init__(self):
         self.list = []
+        self.list_reversed = []
 
     def move(self, duration, power):
         movement = (duration, power)
         self.list.append(movement)
 
     def turn(self, angle, direction, power):
-        angle = -angle
-        direction = -direction
+        if direction == 1:
+            direction = 0
+        else:
+            direction = 1
         movement = (angle, direction, power)
         self.list.append(movement)
 
     def retrace(self):
+        self.list_reversed = self.list[::-1]
+        self.list = []
         return self.list
