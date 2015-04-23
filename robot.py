7e7029f46fc4109edb2933ebaa013e8498f3d57e
cly_js1 <cly_js1@srobo.org>
2015-04-23 11:56:54 +0000

    Added docstrings

--- robot.py
+++ robot.py
@@ -9,16 +9,19 @@ log = Logger()
 R = Robot()
 
 def stage_one(robot, log, power):
+    """Moves forward to first marker, pushes into zone and then releases it"""
     drive_to_marker(robot, log, power)
     drive(robot, log, 1, power)
     release_marker(robot, power)
     
 def saftey_code(robot,log, power):
+    """Hardcoded to drive first marker into wall"""
     drive_distance(robot, log, 2, power)
     turn(robot, log, 45, power)
     drive(robot,log, 3, power)
 
 def test(robot, log, power):
+    """A test function"""
     stage_one(robot, log, power)
 
 test(R, log, 100)
