c21c43fbaf5d16ed6107b1ed767474f90235882b
cly_jt1 <cly_jt1@srobo.org>
2015-04-25 11:04:55 +0000

    Added rotation to search code

--- strategy.py
+++ strategy.py
@@ -28,21 +28,39 @@ def wander(robot, log, power):
     print "Turn Completed"
     Marker = False
     print "Starting Loop"
+    state = 1
     while not Marker:
         m = try_find_flag(robot)
         if m.info.marker_type == MARKER_FLAG:
             Marker = True
             print "Marker Seen"
-        else:
-            drive(robot, log, 1, 100)
+        elif state == 1:
+            drive(robot, log, 1, power)
+            print "No Marker Seen"#
+            state = 2
+        elif state == 2:
+            turn(robot, log, 90, power)
             print "No Marker Seen"
+            state = 3
+        elif state == 3:
+            turn(robot, log, 90, power)
+            print "No Marker Seen"
+            state = 4
+        elif state == 4:
+            turn(robot, log, 90, power)
+            print "No Marker Seen"
+            state = 5
+        elif state == 5:
+            turn(robot, log, 90, power)
+            print "No Marker Seen"
+            state = 1
     print "Finished Loop"
     print "Driving to Marker"
     drive_to_marker(robot, log, power)
     print "Driven to Marker"
     print "Retracing Steps"
     retrace(robot, log, power)
-    print("Steps Retraced, Program Terminating")
+    "Steps Retraced, Program Terminating"
     
     
 def carpet_test(robot, log, power):
