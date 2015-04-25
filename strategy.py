3cc9fbb9f4d3aa0797375cb3ca2fd25433a658d3
cly_jt1 <cly_jt1@srobo.org>
2015-04-25 09:28:14 +0000

    Created wander()

--- strategy.py
+++ strategy.py
@@ -10,7 +10,25 @@ def drive_to_marker(robot, log, power):
     print("Drive to marker")
     marker = find_flag(robot)
     drive_to(robot, log, marker, power)
+
+def wander(robot, log, power):
+    '''Drives into zone to the right'''
+    drive(robot, log, 3, -100)
+    turn(robot, log, 45, 100)
+    drive(robot, log, 3, 100)
+    turn(robot, log, -45, 100)
+    Marker = False
+    while not Marker:
+        m = try_find_flag(robot)
+        if m.info.marker_type != MARKER_FLAG:
+            drive(robot, log, 1, 100)
+        else:
+            Marker = True
+    drive_to_marker(robot, log, power)
+    retrace(robot, log, power)
     
+
+
 def carpet_test(robot, log, power):
     drive(robot, log, 4, power)
     sleep(1)
