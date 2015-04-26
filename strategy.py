0e95ea3f1df59ede4339420c454515805231b3bb
cly_js1 <cly_js1@srobo.org>
2015-04-26 14:28:41 +0000

    Made wander prettier by placing code into functions

--- strategy.py
+++ strategy.py
@@ -18,10 +18,8 @@ def half_drive_to_marker(robot, log, power):
     marker = find_flag(robot)
     half_drive_to(robot, log, marker, power)
     
-
-def wander(robot, log, power):
-    '''Drives into zone to the right'''
-    print("Running wander()")
+    
+def reverse_saftey_code(robot, log, power):
     print("Driving backward")
     drive(robot, log, 2, -power)
     sleep(1)
@@ -40,49 +38,13 @@ def wander(robot, log, power):
     print("Turn Completed")
     drive(robot, log, 4, power)
     sleep(1)
-    
-    Marker = False
-    print("Starting Loop")
-    state = 1
-    while not Marker:
-        m = try_find_flag(robot)
-        try:
-            if m.info.marker_type == MARKER_FLAG:
-                Marker = True
-                print("Marker Seen")   
-        except:
-            if state == 1:
-                drive(robot, log, 1, power)
-                print("No Marker Seen")
-                state = 2
-            elif state == 2:
-                turn(robot, log, 90, power)
-                print("No Marker Seen")
-                state = 3
-            elif state == 3:
-                turn(robot, log, 90, power)
-                print("No Marker Seen")
-                state = 4
-            elif state == 4:
-                turn(robot, log, 90, power)
-                print("No Marker Seen")
-                state = 5
-            elif state == 5:
-                turn(robot, log, 90, power)
-                print("No Marker Seen")
-                state = 1
-            sleep(1)
-        print("Finished Loop 2")
-        
-    print("Driving to Marker")
+
+def wander(robot, log, power):
+    '''Drives into zone to the right'''
+    reverse_saftey_code(robot, log, power)
+    search_marker(robot, 15, log, power)
     drive_to_marker(robot, log, power)
-    print("Driven to Marker")
-    print("Turning 180")
-    turn_old(robot, 180, 1,  power)
-    print("Turned")
-    print("Retracing Steps")
     retrace(robot, log, power)
-    print("Steps Retraced, Program Terminating")
     
     
 def search_marker(robot, increment, log, power):
@@ -209,6 +171,7 @@ def drive_into_barrier(robot, log, power):
     drive(robot, log, 5, power)
     
 def retrace(robot, log, power):
+    turn_old(robot, 180, 1, 50)
     actions = log.retrace()
     for action in actions:
         try:
