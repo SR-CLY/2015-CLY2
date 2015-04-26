86552de3d8f1d03b887d6d168be7610378cb5e73
cly_js1 <cly_js1@srobo.org>
2015-04-26 08:14:20 +0000

    Turn more in stage_one

--- robot.py
+++ robot.py
@@ -18,7 +18,7 @@ def stage_one(robot, log, power):
     sleep(0.1)
     drive_to_marker(robot, log, power)
     sleep(0.1)
-    turn_old(robot, 190, 1, power)
+    turn_old(robot, 200, 1, power)
     sleep(0.1)
     drive(robot, log, 4, power)
     print(log.retrace)
