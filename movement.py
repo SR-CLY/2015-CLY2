8cd91c951536d5a4fba5d2a5bba5088ef3a9964b
sedders123 <sedders123@gmail.com>
2015-04-27 10:35:34 +0100

    Updated docstring

--- movement.py
+++ movement.py
@@ -113,7 +113,7 @@ def drive_to(robot, log, marker, power=100):
     drive_distance(robot, log, distance, power)
     
 def half_drive_to(robot, log, marker, power=100):
-    """Drives to a specified marker"""
+    """Drives halfway to a specified marker"""
     distance = marker.centre.polar.length / 2
     angle = marker.centre.polar.rot_y
     turn(robot, log, angle, power)
